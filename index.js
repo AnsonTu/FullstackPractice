0d537c7c2d679586b5d1d7652e1db78ebce43f90
Anson Tu <anson9137@gmail.com>
2020-01-14 15:47:50 -0500

    Initial MongoDB setup

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // Import the express library using common js module
 const express = require("express");
+// Import mongoose to connect to a Mongo database
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 // Import the passport configuration.
 // We're not assigning anything, so we can just require the file.
 require("./services/passport");
 // Import the authenticated routes handler
 const authRoutes = require("./routes/authRoutes");
+
+// Connect to MongoDB
+mongoose.connect(keys.mongoURI);
 
 // Generate new express app
 const app = express();
