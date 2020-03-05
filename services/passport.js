f0a1df7d66aa60beab1c4ad34ba57db1936518dc
Anson Tu <anson9137@gmail.com>
2020-03-05 11:56:21 -0500

    Added methods for saving userID in the session

--- services/passport.js
+++ services/passport.js
@@ -8,6 +8,16 @@ const keys = require("../config/keys");
 
 const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 // Tell the passport library to use the new Google Strategy
 passport.use(
   new GoogleStrategy(
